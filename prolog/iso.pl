8ee1b65a7f514286e95d51931e53e1df62df449b
Falco Nogatz <fnogatz@gmail.com>
2018-10-29 23:35:18 +0100

    Reformat source code

--- prolog/iso.pl
+++ prolog/iso.pl
@@ -5,12 +5,12 @@
 
 iso_operator(1200, xfx, ':-').
 iso_operator(1200, xfx, '-->').
-iso_operator(1200, fx, ':-').
-iso_operator(1200, fx, '?-').
+iso_operator(1200,  fx, ':-').
+iso_operator(1200,  fx, '?-').
 iso_operator(1100, xfy, ';').
 iso_operator(1050, xfy, '->').
 iso_operator(1000, xfy, ',').
-iso_operator( 900, fy, '\\+').
+iso_operator( 900,  fy, '\\+').
 iso_operator( 700, xfx, '=').
 iso_operator( 700, xfx, '\\=').
 iso_operator( 700, xfx, '==').
@@ -40,8 +40,8 @@ iso_operator( 400, yfx, '<<').
 iso_operator( 400, yfx, '>>').
 iso_operator( 200, xfx, '**').
 iso_operator( 200, xfy, '^').
-iso_operator( 200, fy, '-').
-iso_operator( 200, fy, '\\').
+iso_operator( 200,  fy, '-').
+iso_operator( 200,  fy, '\\').
 
 iso_operators(Ops) :-
   findall(op(Prec,Spec,Name), iso_operator(Prec,Spec,Name), Ops).
