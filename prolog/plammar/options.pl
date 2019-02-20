3b70bfd1ce7b7170013ec638e85f69cded0573d9
Falco Nogatz <fnogatz@gmail.com>
2019-02-20 03:13:24 +0100

    Change the default options of prolog_parsetree

--- prolog/plammar/options.pl
+++ prolog/plammar/options.pl
@@ -6,10 +6,27 @@
 
 normalise_options(prolog_parsetree, User_Options, Options) :-
   default_options(prolog_parsetree, Default_Options),
-  merge_options(User_Options, Default_Options, Options).
+  merge_options(User_Options, Default_Options, Options0),
+  ( option(iso_operators(X), Options0), yes(X) ->
+    iso_operators(ISO_Operators),
+    option(operators(User_Operators), Options0),
+    merge_operators(ISO_Operators, User_Operators, Operators),
+    merge_options([operators(Operators)], Options0, Options1)
+  ; Options1 = Options0 ),
+  Options = Options1.
 
 default_options(prolog_parsetree, Options) :-
-  iso_operators(ISO_Operators),
   Options = [
-    operators(ISO_Operators)
+    operators([]),
+    iso_operators(yes),
+    infer_operators(no)
   ].
+
+yes(yes).
+yes(true).
+yes(y).
+yes(ok).
+
+merge_operators(Old, New, Merged) :-
+  %% TODO!
+  append(Old, New, Merged).
--- test/predicates/prolog_parsetree.pl
+++ test/predicates/prolog_parsetree.pl
@@ -44,8 +44,8 @@
     p_text([directive_term([term(xfx,[term(atom(name([name_token(letter_digit_token([small_letter_char(a)]))]))),op(atom(name([layout_text_sequence([layout_text(layout_char(space_char(' ')))]),name_token(graphic_token([graphic_token_char(graphic_char(':')),graphic_token_char(graphic_char('-'))]))]))),term(atom(name([layout_text_sequence([layout_text(layout_char(space_char(' ')))]),name_token(letter_digit_token([small_letter_char(b)]))])))]),end([end_token(end_char('.'))])])])
   ].
 
-'"a :- b." cannot be parsed when no operators are pre-defined'(fail) :-
-  prolog_parsetree(string("a :- b."), _PT, [ operators([]) ]).
+'"a :- b." cannot be parsed when ISO operators are not used'(fail) :-
+  prolog_parsetree(string("a :- b."), _PT, [ iso_operators(no) ]).
 
 '"a b c." can be parsed with the appropriate operator' :-
   % try all variants xfx, xfy, and yfx
@@ -53,6 +53,33 @@
     prolog_parsetrees(string("a b c."), PTs, [ operators([ op(800,Type,b) ]) ]),
     length(PTs, 1)
   ), [xfx, xfy, yfx]).
+
+'"f :- a." is valid for f not being an operator' :-
+  Options = [],
+  prolog_parsetrees(string("f :- a."), PTs, Options),
+  length(PTs, 1).
+
+/*
+'"f :- a." is valid for f being an operator fy@1100' :-
+  Options = [ operators([ op(1100,fy,f) ]) ],
+  prolog_parsetrees(string("f :- a."), PTs, Options),
+  length(PTs, 1).
+
+'"f :- a." is valid for f being an operator fx@1100' :-
+  Options = [ operators([ op(1100,fx,f) ]) ],
+  prolog_parsetrees(string("f :- a."), PTs, Options),
+  length(PTs, 1).
+
+'"f :- a." is valid for f being an operator fy@1200' :-
+  Options = [ operators([ op(1200,fy,f) ]) ],
+  prolog_parsetrees(string("f :- a."), PTs, Options),
+  length(PTs, 1).
+
+'"f :- a." is not valid for f being an operator fx@1200' :-
+  Options = [ operators([ op(1200,fx,f) ]) ],
+  prolog_parsetrees(string("f :- a."), PTs, Options),
+  length(PTs, 0).
+*/
 
 %% Part II: parse tree given, string unbound
 
